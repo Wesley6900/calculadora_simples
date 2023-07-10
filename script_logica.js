e98b7eb1d3fbc5ea5c4b59572d3e3ea60f13ef8c
Wesley6900 <mcwesley147@gmail.com>
2023-07-10 17:26:34 -0300

    calculadora

--- script_logica.js
+++ script_logica.js
@@ -1,155 +1,137 @@
-var receber_p_enviar = window.document.getElementById('textarea');
-
-
+var receber_value_vetor;
+var resultado1;
+var receber_p_enviar = window.document.getElementById("textarea");
 var vetor_global_1 = [];
 var vetor_global_2 = [];
+var simb_indi = "";
 
-var receber_value_vetor;
-var simb_indi = ''
-var resultado1;
-
-receber_p_enviar.innerHTML = receber_value_vetor == void(0) ? '0': receber_value_vetor;
+receber_p_enviar.innerHTML = null == receber_value_vetor ? "0" : receber_value_vetor;
 
 function apagar() {
-    reset();
+    for (reset(), i = 0; ; i++){
+        if (vetor_global_2.pop([i]), 0 == vetor_global_2.length){
 
-    for(i = 0; ; i ++){
-        vetor_global_2.pop([i]);
-        if (vetor_global_2.length == 0){
             break;
         }
+        receber_value_vetor = 0;
+        receber_p_enviar.innerHTML = 0;
     }
-
-    receber_value_vetor = 0;
-    receber_p_enviar.innerHTML = 0
 }
 
 function apagar_individual() {
-
-    if (receber_value_vetor < 0) {
-        for(let i = vetor_global_1.length; ;i -= 1){
-            vetor_global_1.pop([i]);
-    
+    if (0 > receber_value_vetor){
+        for (let a = vetor_global_1.length; ; a -= 1) {
+            vetor_global_1.pop([a]);
             receber_value_vetor = vetor_global_1;
-            receber_value_vetor = receber_value_vetor.join('');
-            receber_value_vetor = (receber_value_vetor - receber_value_vetor) - receber_value_vetor;
+            receber_value_vetor = receber_value_vetor.join("");
+            receber_value_vetor = receber_value_vetor - receber_value_vetor - receber_value_vetor;
             receber_p_enviar.innerHTML = receber_value_vetor;
-    
-            if (vetor_global_1[0] == void(0)) {
+
+            if (null == vetor_global_1[0]) {
                 receber_p_enviar.innerHTML = 0;
             }
 
             break;
         }
     }else{
-        for(let i = vetor_global_1.length; ;i -= 1){
-            vetor_global_1.pop([i]);
-    
+        for (let a = vetor_global_1.length; ; a -= 1) {
+            vetor_global_1.pop([a]);
             receber_value_vetor = vetor_global_1;
-            receber_value_vetor = receber_value_vetor.join('');
+            receber_value_vetor = receber_value_vetor.join("");
             receber_p_enviar.innerHTML = receber_value_vetor;
-    
-            if (vetor_global_1[0] == void(0)) {
+
+            if (null == vetor_global_1[0]) {
                 receber_p_enviar.innerHTML = 0;
             }
-    
+            
             break;
         }
     }
 }
 
 function deixar_negativo_one() {
-
-    receber_value_vetor = (receber_value_vetor - receber_value_vetor) - receber_value_vetor;
+    receber_value_vetor = receber_value_vetor - receber_value_vetor - receber_value_vetor;
     receber_p_enviar.innerHTML = receber_value_vetor;
 }
 
-function Primeiro_valor(recebe_valores){
-    var recebe_valores;
-    
-    switch (recebe_valores) {
-        case '+':
-            simb_indi = '+';
+function Primeiro_valor(a) {
+    var a;
+    switch (a) {
+        case "+":
+            simb_indi = "+";
             vetor_global_2.push(receber_value_vetor);
             reset();
 
             break;
 
-        case '/':
-            simb_indi = '/';
+        case "/":
+            simb_indi = "/";
             vetor_global_2.push(receber_value_vetor);
             reset();
 
             break;
 
-        case 'X':
-            simb_indi = 'X';
+        case "X":
+            simb_indi = "X";
             vetor_global_2.push(receber_value_vetor);
             reset();
 
             break;
 
-        case '-':
-            simb_indi = '-';
+        case "-":
+            simb_indi = "-";
             vetor_global_2.push(receber_value_vetor);
             reset();
-    
+
             break;
-        case 'ce':
+
+        case "ce":
             apagar_individual();
-
             break;
-
         default:
-            vetor_global_1.push(recebe_valores);
+            vetor_global_1.push(a);
             receber_value_vetor = vetor_global_1;
-            receber_value_vetor = receber_value_vetor.join(''); /*serve para remover virgulas*/ 
-            receber_p_enviar.innerHTML = receber_value_vetor; /*Printar valor*/
-
-            break;
-    }
+            receber_value_vetor = receber_value_vetor.join("");
+            receber_p_enviar.innerHTML = receber_value_vetor;
+    } 
 }
-function reset(){ 
-    for(i = 0; ; i ++){
-        vetor_global_1.pop([i]);
-        if (vetor_global_1.length == 0){
+
+function reset() {
+    for (i = 0; ; i++){
+        if (vetor_global_1.pop([i]), 0 == vetor_global_1.length){
             break;
         }
     }
 }
 
-
-function calcular_resultado(){
-
+function calcular_resultado() {
     switch (simb_indi) {
-        case '+':
-            resultado1 = Number.parseFloat(vetor_global_2[0]) + Number.parseFloat(receber_value_vetor);
-            receber_p_enviar.innerHTML = vetor_global_2[0] + ' + '+ receber_value_vetor  + ' = ' + Number.parseFloat(resultado1);
+      case "+":
+          resultado1 = Number.parseFloat(vetor_global_2[0]) + Number.parseFloat(receber_value_vetor);
+          receber_p_enviar.innerHTML =
+          vetor_global_2[0] + " + " + receber_value_vetor + " = " + Number.parseFloat(resultado1);
 
             break;
-
-        case '/':
-            resultado1 = Number.parseFloat(vetor_global_2[0]) / Number.parseFloat(receber_value_vetor);
-            receber_p_enviar.innerHTML = vetor_global_2[0] + ' / '+ receber_value_vetor  + ' = ' + Number.parseFloat(resultado1);
-            
-            break;
-
-        case 'X':
-            resultado1 = Number.parseFloat(vetor_global_2[0]) * Number.parseFloat(receber_value_vetor);
-            receber_p_enviar.innerHTML = vetor_global_2[0] + ' x '+ receber_value_vetor  + ' = ' + Number.parseFloat(resultado1);
-            
-            break;
-
-        case '-':
-            resultado1 = Number.parseFloat(vetor_global_2[0]) - Number.parseFloat(receber_value_vetor);
-            receber_p_enviar.innerHTML = vetor_global_2[0] + ' - '+ receber_value_vetor  + ' = ' + Number.parseFloat(resultado1);
+      case "/":
+          resultado1 = Number.parseFloat(vetor_global_2[0]) / Number.parseFloat(receber_value_vetor);
+          receber_p_enviar.innerHTML =
+          vetor_global_2[0] + " / " + receber_value_vetor + " = " + Number.parseFloat(resultado1);
 
             break;
+      case "X":
+          resultado1 = Number.parseFloat(vetor_global_2[0]) * Number.parseFloat(receber_value_vetor);
+          receber_p_enviar.innerHTML =
+          vetor_global_2[0] + " x " + receber_value_vetor + " = " + Number.parseFloat(resultado1);
 
-        default:
-            window.document.alert('Digite um valor valido!!!')
             break;
+      case "-":
+          resultado1 = Number.parseFloat(vetor_global_2[0]) - Number.parseFloat(receber_value_vetor);
+          receber_p_enviar.innerHTML =
+          vetor_global_2[0] + " - " + receber_value_vetor + " = " + Number.parseFloat(resultado1);
+
+            break;
+      default:
+            window.document.alert("Digite um valor válido!!!");
     }
-
     receber_value_vetor = resultado1;
 }
